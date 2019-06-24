9500c6e435101c1fc050e225a8a1627aaa8b3edf
Katya3141 <katya3141@gmail.com>
2019-06-24 14:47:49 -0400

    initial working version

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -24,7 +24,8 @@ const builtinExtensions = {
     ev3: () => require('../extensions/scratch3_ev3'),
     makeymakey: () => require('../extensions/scratch3_makeymakey'),
     boost: () => require('../extensions/scratch3_boost'),
-    gdxfor: () => require('../extensions/scratch3_gdx_for')
+    gdxfor: () => require('../extensions/scratch3_gdx_for'),
+    test: () => require('../extensions/scratch3_test')
 };
 
 /**
